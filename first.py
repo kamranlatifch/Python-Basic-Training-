41814dc5c58d3ebe446c786fa82fc57058bc59b8
Kamran Latif Ch <cogentlabs@Kamrans-MacBook-Air.local>
2025-12-10 10:49:37 +0500

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+
+a = 5
+
+print("kamram", "latif", sep="-")
+# python as calculator
+
+print(10 + 20)
+print(10 - 20)
+print(10 * 20)
+print(10 / 20)
+print(10 % 20)
+print(10**20)
+print(10 // 20)
+print(10**20)
+print(10**20)
