3799a4e6c73e83e9952e5fad8d29caa2c54adcec
Kamran Latif Ch <cogentlabs@Kamrans-MacBook-Air.local>
2025-12-24 18:11:47 +0500

    add

--- Basic Flow Control Task/guessing_number.py
+++ Basic Flow Control Task/guessing_number.py
@@ -1,7 +1,12 @@
+import random
+
+
 def guessing_game():
-    target_number = 58
+
     attempts = 0
     is_guessed = False
+
+    target_number = random.randint(1, 100)
 
     while not is_guessed:
         try:
@@ -23,6 +28,7 @@ def guessing_game():
         else:
             print("Correct!")
             is_guessed = True
+            break
 
     print(f"Number of attempts: {attempts}")
 
