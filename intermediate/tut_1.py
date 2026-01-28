85437e68e6fb2e7c44705624dfd9d27157f953d6
Kamran Latif Ch <cogentlabs@Kamrans-MacBook-Air.local>
2026-01-28 18:48:23 +0500

    add: learn __main__

---
+++ intermediate/tut_1.py
@@ -0,0 +1,14 @@
+import os
+
+
+def print_hello():
+    print("Hello World")
+
+
+def main():
+    print(os.listdir("/"))
+    print("Lets Learn __main__")
+
+
+if __name__ == "__main__":
+    main()
---
+++ intermediate/tut_2.py
@@ -0,0 +1,4 @@
+import tut_1
+
+tut_1.print_hello()
+tut_1.main()
